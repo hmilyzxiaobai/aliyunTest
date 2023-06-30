2820eec6621a759971d822adfe59ff996e31978c
zhangll4 <zlldream@hotamil.com>
2023-06-30 17:35:21 +0800

    抢票的代码

--- src/main/java/example/controller/HelloWorld.java
+++ src/main/java/example/controller/HelloWorld.java
@@ -1,5 +1,7 @@
 package example.controller;
 
+import example.util.RobTicketWithDaMai;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorld {
     @RequestMapping("yeap")
     public String yeap(){
+        return "成功打通网络";
+    }
+
+    @Autowired
+    private RobTicketWithDaMai robTicketWithDaMai;
+    @RequestMapping("runTicket")
+    public String runTicket(){
+        robTicketWithDaMai.start();
         return "成功打通网络";
     }
 }
