334ed1eb37dc44fb0ffa8c54a856f54bab0d0c0d
zhangll4 <zlldream@hotamil.com>
2023-08-06 14:01:12 +0800

    测试发布到阿里云

--- src/main/java/example/controller/HelloWorld.java
+++ src/main/java/example/controller/HelloWorld.java
@@ -1,16 +1,40 @@
 package example.controller;
 
 
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.context.request.WebRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 @RestController
 @RequestMapping("hello")
+@Slf4j
 public class HelloWorld {
     @RequestMapping("yeap")
     public String yeap(){
-        return "成功打通网络ss";
+    log.info("请求访问");
+        //获取访问的http端口信息等
+      //  获取访问的http端口信息等
+        //获取访问的http端口信息等
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        String url = request.getRequestURL().toString();
+        String method = request.getMethod();
+        String ip = request.getRemoteAddr();
+        int port = request.getRemotePort();
+        SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd  HH:mm:ss z");
+        Date date = new Date(System.currentTimeMillis());
+        System.out.println(formatter.format(date));
+        log.info("该网络访问基本信息为：url:{} ,method:{} ,ip:{} ,port:{};访问时间为：{}",url,method,ip,port,formatter.format(date));
+        return "欢迎访问";
     }
 
 
