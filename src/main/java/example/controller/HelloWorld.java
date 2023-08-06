f78a2324141390da3c951f5dc5f9192599de532a
zhangll4 <zlldream@hotamil.com>
2023-08-06 13:29:48 +0800

    测试发布到阿里云

--- src/main/java/example/controller/HelloWorld.java
+++ src/main/java/example/controller/HelloWorld.java
@@ -1,9 +1,9 @@
 package example.controller;
 
-import example.util.RobTicketWithDaMai;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("hello")
@@ -13,11 +13,6 @@ public class HelloWorld {
         return "成功打通网络";
     }
 
-    @Autowired
-    private RobTicketWithDaMai robTicketWithDaMai;
-    @RequestMapping("runTicket")
-    public String runTicket(){
-        robTicketWithDaMai.start();
-        return "成功打通网络";
-    }
+
+
 }
