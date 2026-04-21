c6b118188dc9d2a92711ebebba6e0961b37cf9a8
zlldream@hotmail.com <zlldream@hotmial.com>
2026-04-21 14:19:02 +0800

    增加使用个人监控版

--- src/main/java/example/dongfangcaifu/service/MonitoringPersonService.java
+++ src/main/java/example/dongfangcaifu/service/MonitoringPersonService.java
@@ -3,7 +3,11 @@ package example.dongfangcaifu.service;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import example.dongfangcaifu.mapper.MonitoringPersonMapper;
 import example.dongfangcaifu.src.entity.MonitoringPersonEntity;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+@Service
+@Slf4j
 public class MonitoringPersonService extends ServiceImpl<MonitoringPersonMapper, MonitoringPersonEntity> {
 
 }
